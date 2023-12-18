342a756c3a9de00259fccc4fa3ab328e01404519
mnadithya <adithya@webtrigon.com>
2023-12-18 13:03:26 +0530

    chant + explore card fixed for all reso

--- src/components/ChantCard/ChantCard.tsx
+++ src/components/ChantCard/ChantCard.tsx
@@ -13,16 +13,18 @@ export const ChantCard = () => {
   }
 
   return (
-    <IonCard className={styles.chantCard}>
-      <IonThumbnail className={styles.chantImage}><img src={ChantImage} alt="" /></IonThumbnail>
-      <div className={styles.chantText}>
-        <span className={styles.chantTitle}>Today's Chant</span>
-        <span className={styles.chantSubtitle}>A daily chant for focused mindfullness and positive intention.</span>
-        <button className={styles.chantButton} onClick={handleChantClick}>
-          <IonThumbnail className={styles.chantPlayicon}><img src={ChantPlayIcon} alt="play button" /></IonThumbnail>
-          <span className={styles.chantPlayText}>Start Today's Chant</span>
-        </button>
-      </div>
-    </IonCard>
+    <div className={styles.chantCardContainer}>
+      <IonCard className={styles.chantCard}>
+        <IonThumbnail className={styles.chantImage}><img src={ChantImage} alt="" /></IonThumbnail>
+        <div className={styles.chantText}>
+          <span className={styles.chantTitle}>Today's Chant</span>
+          <span className={styles.chantSubtitle}>A daily chant for focused mindfullness and positive intention.</span>
+          <button className={styles.chantButton} onClick={handleChantClick}>
+            <IonThumbnail className={styles.chantPlayicon}><img src={ChantPlayIcon} alt="play button" /></IonThumbnail>
+            <span className={styles.chantPlayText}>Start Today's Chant</span>
+          </button>
+        </div>
+      </IonCard>
+    </div>
   )
 }
--- src/components/ExploreCard/ExploreCard.tsx
+++ src/components/ExploreCard/ExploreCard.tsx
@@ -5,16 +5,18 @@ import styles from './ExploreCard.module.scss';
 
 export const ExploreCard = () => {
   return (
-    <IonCard className={styles.exploreCard}>
-      <div className={styles.exploreText}>
+    <div className={styles.exploreCardContainer}>
+      <IonCard className={styles.exploreCard}>
+        <div className={styles.exploreText}>
           <span className={styles.exploreTitle}>Explore Temple</span>
           <span className={styles.exploreSubtitle}>Discover The Essence of a Journey of Spiritual Awakening.</span>
           <button className={styles.exploreButton}>
             <IonThumbnail className={styles.explorePlayicon}><img src={ExplorePlayIcon} alt="play button" /></IonThumbnail>
             <span className={styles.explorePlayText}>Watch live</span>
           </button>
-      </div>
-      <IonThumbnail className={styles.exploreImage}><img src={ExploreImage} alt="temple image" /></IonThumbnail>
-    </IonCard>
+        </div>
+        <IonThumbnail className={styles.exploreImage}><img src={ExploreImage} alt="temple image" /></IonThumbnail>
+      </IonCard>
+    </div>
   )
 }
