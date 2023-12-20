2d8e24e4ba3e2889fc1d602212ed3ef05986abf5
mnadithya <adithya@webtrigon.com>
2023-12-20 13:45:16 +0530

    background added instead of separate overlays

--- src/pages/TrendingPages/TrendingPlaceHolder.tsx
+++ src/pages/TrendingPages/TrendingPlaceHolder.tsx
@@ -1,22 +1,14 @@
 import { IonContent, IonIcon, IonPage } from "@ionic/react";
-import { useHistory } from "react-router";
+import { Header } from "../../components/Header/Header";
+
 import styles from './TrendingPlaceHolder.module.scss'
 
-import BackIcon from '../../assets/icons/arrow-back-outline.svg'
 
 export const TrendingPlaceHolder = () => {
-    const history = useHistory();
-
-    const handleBackIconClick = () => {
-        history.push('/')
-    }
-
     return (
         <IonPage>
             <IonContent fullscreen>
-                <div className={styles.navBackIcon} onClick={handleBackIconClick}>
-                    <IonIcon icon={BackIcon}></IonIcon>
-                </div>
+                <Header greeting=""/>
                 <div className={styles.trendPageHeader}>Trending Topic</div>
                 <div className={styles.trendPageText}>
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas, totam autem! Eum, magnam dolorum consequatur earum exercitationem pariatur optio nobis eaque aspernatur illo odio ipsa atque voluptatibus odit quas temporibus.
